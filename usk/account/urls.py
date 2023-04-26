0c6f42b6cb9ad44db76a08f70d219fec8cfd89d7
morskmorsk <khalidfour@yahoo.com>
2023-04-25 20:49:08 -0500

    fix profile menu

--- usk/account/urls.py
+++ usk/account/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from . import views
+from .views import profile, ProfileEditView, ChangePasswordView, signup
 
 app = 'account'
 
 urlpatterns = [
-    # ... other URL patterns ...
-    path('signup/', views.signup, name='signup'),
+    path('signup/', signup, name='signup'),
+    path('profile/', profile, name='profile'),
+    path('profile/edit/', ProfileEditView.as_view(), name='profile_edit'),
+    path('profile/change', ChangePasswordView.as_view(), name='change_password'),
 ]
--- usk/account/views.py
+++ usk/account/views.py
@@ -1,6 +1,13 @@
+from django.contrib.auth.forms import UserChangeForm, PasswordChangeForm
 from django.contrib.auth import login
 from .forms import SignupForm
 from django.shortcuts import render, redirect
+# views.py
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import update_session_auth_hash
+from django.views import View
+from django.contrib import messages
 
 
 def signup(request):
@@ -9,9 +16,45 @@ def signup(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            # Change 'home' to the name of the view you want to
-            # redirect to after successful registration
             return redirect('shopping_cart:product_list')
     else:
         form = SignupForm()
     return render(request, 'signup.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'account/profile.html', {'user': request.user})
+
+
+class ProfileEditView(LoginRequiredMixin, View):
+    def get(self, request):
+        form = UserChangeForm(instance=request.user)
+        return render(request, 'account/profile_edit.html', {'form': form})
+
+    def post(self, request):
+        form = UserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your profile has been updated.')
+            return redirect('profile')
+        else:
+            messages.error(request, 'Please correct the errors below.')
+        return render(request, 'account/profile_edit.html', {'form': form})
+
+
+class ChangePasswordView(LoginRequiredMixin, View):
+    def get(self, request):
+        form = PasswordChangeForm(user=request.user)
+        return render(request, 'account/change_password.html', {'form': form})
+
+    def post(self, request):
+        form = PasswordChangeForm(data=request.POST, user=request.user)
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)
+            messages.success(request, 'Your password has been updated.')
+            return redirect('profile')
+        else:
+            messages.error(request, 'Please correct the errors below.')
+        return render(request, 'account/change_password.html', {'form': form})
