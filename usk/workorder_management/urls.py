8a0e7ad450f2edeba728f50082c6b81da12f79f3
morskmorsk <khalidfour@yahoo.com>
2023-04-26 22:29:56 -0600

    add workorder_management urls

--- usk/workorder_management/urls.py
+++ usk/workorder_management/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
+    # other URLs ...
+
+    path('locations/', views.LocationListView.as_view(),
+         name='location_list'),
+    path('devices/', views.DeviceListView.as_view(),
+         name='device_list'),
+    path('workorders/', views.WorkOrderListView.as_view(),
+         name='workorder_list'),
 ]
--- usk/workorder_management/views.py
+++ usk/workorder_management/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Location, Device, WorkOrder
 
-# Create your views here.
+
+class LocationListView(LoginRequiredMixin, ListView):
+    model = Location
+    template_name = 'locations/location_list.html'
+    context_object_name = 'locations'
+
+
+class DeviceListView(LoginRequiredMixin, ListView):
+    model = Device
+    template_name = 'devices/device_list.html'
+    context_object_name = 'devices'
+
+
+class WorkOrderListView(LoginRequiredMixin, ListView):
+    model = WorkOrder
+    template_name = 'workorders/workorder_list.html'
+    context_object_name = 'workorders'
