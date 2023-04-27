9b39300d6c8cffb2e5765ab0b0c34a2bddef9ba5
morskmorsk <khalidfour@yahoo.com>
2023-04-26 21:16:10 -0500

    fix workorder models

--- usk/workorder_management/models.py
+++ usk/workorder_management/models.py
@@ -1,4 +1,3 @@
-from decimal import Decimal
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -15,47 +14,48 @@ class Location(models.Model):
 
 class Device(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    device_model = models.CharField(max_length=25)
-    imei = models.CharField(max_length=20)
+    device_model = models.CharField(max_length=25, blank=True, null=True)
+    imei = models.CharField(max_length=20, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
-    defect = models.CharField(max_length=255)
-    estimated_repair_price = models.DecimalField(max_digits=10, decimal_places=2)
+    defect = models.CharField(max_length=255, blank=True, null=True)
+    estimated_repair_price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     cost_of_repair = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     part_cost = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     location = models.ForeignKey(Location, on_delete=models.CASCADE, blank=True, null=True)
     image = models.ImageField(
         upload_to='static/images/product_images/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_repaired = models.BooleanField(default=False)
+    is_repairable = models.BooleanField(default=True)
 
     def __str__(self):
-        return self.name
+        return self.owner.username + "'s " + self.device_model
+
+    def get_total_cost(self):
+        return self.estimated_repair_price + self.part_cost
 
 
 class WorkOrder(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.user.username}'s Work Order"
 
-    def get_sales_tax(self):
-        tax_rate = 0.09  # 9% sales tax rate
-        taxable_items = WorkOrderItem.objects.filter
-        total_taxable = sum(
-            item.price * item.quantity for item in taxable_items)
-        return total_taxable * Decimal(tax_rate)
-
 
 class WorkOrderItem(models.Model):
     device = models.ForeignKey(Device, on_delete=models.CASCADE)
-    cart = models.ForeignKey(WorkOrder, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(default=1)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    workorder = models.ForeignKey(WorkOrder, on_delete=models.CASCADE)
+    repair_price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return f"{self.product.name} x {self.quantity}"
 
 
 class Order(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -65,9 +65,8 @@ class Order(models.Model):
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    device = models.ForeignKey(Device, on_delete=models.CASCADE)
+    repair_price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
-        return f"{self.product.name} x {self.quantity}"
+        return f"{self.device.owner} x {self.device.device_model}"
